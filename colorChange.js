40c9e8cdf4564df60754e235c46242e105773309
shanto <hhassibul@gmail.com>
2022-08-04 21:09:36 +0600

    urgh

---
+++ colorChange.js
@@ -0,0 +1,11 @@
+function chng(){
+root.style.backgroundColor = rgb()
+}
+
+function rgb(){
+  const red = Math.floor(Math.random()*255)
+  const green = Math.floor(Math.random()*255)
+  const blue = Math.floor(Math.random()*255)
+  return `RGB(${red},${green},${blue})`
+}
+console.log(rgb());
