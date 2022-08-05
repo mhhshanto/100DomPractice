d8921c17215165175cfbbe185fc44b59fb44f2ea
shanto <hhassibul@gmail.com>
2022-08-06 03:23:32 +0600

    urgh

---
+++ hexa.js
@@ -0,0 +1,15 @@
+function chng(){
+  root.style.backgroundColor = rgb()
+  }
+  
+  function rgb(){
+    const red = Math.floor(Math.random()*999999)
+    if(red.length = 5){
+
+      continue
+   
+  }
+  
+  return `#${red}}`
+  }
+  console.log(rgb());
